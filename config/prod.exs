189ffa45f4b642fb817935c259e76e0acce459f8
Alexander <web@fuchsberger.us>
2017-08-20 23:58:19 +0000

    0.2.0

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ use Mix.Config
 config :collaboration, Collaboration.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "159.203.137.57", port: 81],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
   # configuration for the Distillery release
   root: ".",
   server: true,
