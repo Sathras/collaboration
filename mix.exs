f0c7a037eb8a2e77de7c91cbb8b8ad0c795c7ad6
Alexander <web@fuchsberger.us>
2017-08-24 19:35:21 +0000

    0.2.14

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Collaboration.Mixfile do
 
   def project do
     [app: :collaboration,
-     version: "0.2.13",
+     version: "0.2.14",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
