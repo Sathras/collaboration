7750fd8354048cd04d27c4ab33b209c7c9884e50
Alexander <web@fuchsberger.us>
2018-04-15 11:36:31 -0500

    2.0.0 Release version

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 
 config :collaboration, CollaborationWeb.Endpoint,
-  http: [port: System.get_env("PORT") || 4002],
+  http: [port: System.get_env("PORT") || 4003],
   url:  [scheme: "https", host: "collaboration.fuchsberger.us", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
